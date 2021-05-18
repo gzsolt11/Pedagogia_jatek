6d7674465f07587c08c980893fa2b9f5f690b859
gzsolt11 <59285476+gzsolt11@users.noreply.github.com>
2021-05-18 21:16:23 +0300

    Update display.js

--- Game/display.js
+++ Game/display.js
@@ -5,36 +5,48 @@ import { getWordRanges } from "./input.js";
 import { Map } from "./map.js";
 import { highlightWordRanges } from "./input.js";
 import { resetTextArea } from "./input.js";
+import {maps} from "./src/map.js";
+import { setStartDirection } from "./input.js"
+import { willCharacterMove } from "./input.js"
 
 var canvas = document.getElementById("canvas");
-//var context = canvas.getContext("2d");
 
+var score = 0;
+
+var userId = 3;
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
+const MAXSCORE = 100;
+var lostScore = 0;
 let lastRenderTime = 0;
 const renderSpeed = 1;
 const playScreen = document.getElementById("playScreen");
 
-let mapIndex = 0;
+export var mapIndex = 0;
+var mapCoordinates = maps[mapIndex]['coordinates'];
+var startDirection = maps[mapIndex]['startDirection'];
+var isReplaceMap =maps[mapIndex]['isReplacing'];
+var baseText = maps[mapIndex]['baseText'];
+var difficulty = maps[mapIndex]['level'];
+setDifficulty(difficulty);
+setStartDirection(startDirection);
+changeCodeType(isReplaceMap,baseText);
 
-var maps = [[{x:2,y:2},{x:2,y:3},{x:2,y:4},{x:3,y:4},{x:4,y:4},{x:4,y:3},{x:4,y:2}],
-            [{x:2,y:2},{x:2,y:3},{x:2,y:4},{x:3,y:4},{x:3,y:5},{x:3,y:6},{x:4,y:6}]]
 
-
-var mapCoordinates = maps[mapIndex];
-
-let character = new Character(GAME_WIDTH,GAME_HEIGHT);
 let map = new Map(mapCoordinates);
+let character = new Character(GAME_WIDTH,GAME_HEIGHT,mapCoordinates[0]['x'],mapCoordinates[0]['y'],startDirection);
 let isFailed = false;
+//character.setRotate(startDirection);
 let moveCounter = 0;
+var lastAngle = character.angle;
 
 
 
 function move(currentTime){
 
    window.requestAnimationFrame(move);
-    
+
     const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
 
     if(secondsSinceLastRender < (1/renderSpeed)){
@@ -55,15 +67,37 @@ function move(currentTime){
         update(direction,angle);
 
         if(character.position.x == mapCoordinates[mapCoordinates.length-1].x && character.position.y == mapCoordinates[mapCoordinates.length-1].y){
-            map.isCharacterAtFinish();
-            document.getElementById("blackTransparentScreen2").style.visibility = "visible";
+            if(!willCharacterMove()){
+                map.isCharacterAtFinish();
+                if(document.getElementById("blackTransparentScreen2").style.visibility != "visible"){
+                    document.getElementById("modalSuccessTemp").innerHTML = '';
+                    const scoreLine = document.createElement('p');
+                    scoreLine.innerHTML = "You'r score: " + (MAXSCORE-lostScore) + "/" +MAXSCORE;
+                    scoreLine.id = "failedTextLine";
+                    document.getElementById("modalSuccessTemp").appendChild(scoreLine);
+                    saveScoreToDatabase(userId,mapIndex,MAXSCORE-lostScore);
+                }
+                document.getElementById("blackTransparentScreen2").style.visibility = "visible";
+                
+                
+            }else{
+                isFailed = true;
+                lostScore += 10;
+                draw();
+                update();
+                showFailedScreen();
+                return;
+            }
         }
         
     }else{
         if(!isFailed){ 
+            lostScore += 10;
             draw();
             update();
-            highlightWordRanges(range);
+            if(maps[mapIndex]['level'] == "beginner"){
+                highlightWordRanges(range);
+            }
             showFailedScreen();
             return;
         }
@@ -72,15 +106,22 @@ function move(currentTime){
 }
 window.requestAnimationFrame(move);
 
-function update(direction={x:0,y:0}, angle=0){
-    character.update(direction,angle);
+function update(direction={x:0,y:0}, angle){
+    if(angle == undefined){
+        if(maps[mapIndex]['level'] == "advanced"){
+            angle = lastAngle;
+        }else{
+            angle = 0;
+        }
+    }
+    character.update(direction,angle,difficulty);
     
 }
-
 
 function draw(range = {start:0,end:0}){
     playScreen.innerHTML='';
     character.draw(playScreen);
+    lastAngle = character.angle;
     map.draw(playScreen);
     if(!(range.start == 0 && range.end == 0)){
         highlightWordRanges(range);
@@ -99,9 +140,12 @@ function restartGame(){
     isFailed = false;
     resetTextArea();
     map.resetFinishTexture();
-    character.setRotate(0);
-    character.position.x = mapCoordinates[0].x;
-    character.position.y = mapCoordinates[0].y;
+    character.setRotate(startDirection);
+    setStartDirection(startDirection);
+    changeCodeType(isReplaceMap,baseText);
+    setDifficulty(difficulty);
+    character.position.x = mapCoordinates[0]['x'];
+    character.position.y = mapCoordinates[0]['y'];
     document.getElementById("inputField").style.background = "rgba(32, 177, 51, 0.89)";
     document.getElementById("blackTransparentScreen").style.visibility = "hidden";
     document.getElementById("blackTransparentScreen2").style.visibility = "hidden";
@@ -118,10 +162,32 @@ for(var i = 0; i < tryAgainButtons.length; ++i){
 var nextMapButton = document.getElementById("nextButton").addEventListener("click",nextMap);
 
 function nextMap(){
-    mapIndex++;
-    mapCoordinates = maps[mapIndex];
-    map = new Map(mapCoordinates);
-    restartGame();
+    if(mapIndex < 5){
+        mapIndex++;
+        mapCoordinates = maps[mapIndex]['coordinates'];
+        startDirection = maps[mapIndex]['startDirection'];
+        isReplaceMap =maps[mapIndex]['isReplacing'];
+        baseText =maps[mapIndex]['baseText'];
+        difficulty = maps[mapIndex]['level'];
+        setDifficulty(difficulty);
+        setStartDirection(startDirection);
+        character.setRotate(startDirection);
+        map = new Map(mapCoordinates);
+        restartGame();
+
+        
+    }
+    var data = new FormData();
+    data.append("score",score);
+    data.append("map_id",mapIndex);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","saveScoreInDB.php");
+    xhr.onload = function(){
+        console.log(this.response);
+    }
+
+    xhr.send(data);
 }
 
 function showFailedScreen(){
@@ -131,5 +197,99 @@ function showFailedScreen(){
     const mistakeLine = document.createElement('p');
     mistakeLine.innerHTML = "You have got a mistake in line: " + (moveCounter);
     mistakeLine.id = "failedTextLine";
-    document.getElementById("modalFailedTextBox").appendChild(mistakeLine);
+    if(maps[mapIndex]['level'] == "beginner"){
+        document.getElementById("modalFailedTemp").innerHTML = '';
+        document.getElementById("modalFailedTemp").appendChild(mistakeLine);
+    }
+    
+}
+
+function changeCodeType(isReplaceMap,baseText){
+    if(isReplaceMap){
+        document.getElementById("inputAndCodeField").style.visibility = "hidden";
+        document.getElementById("replaceField").style.visibility = "visible";
+        writeTextToReplaceCodeField(baseText);
+    }else{
+        document.getElementById("replaceField").style.visibility = "hidden";
+        document.getElementById("inputAndCodeField").style.visibility = "visible";
+    }
+}
+
+function writeTextToReplaceCodeField(text){
+    var field = document.getElementById("replaceCodeField");
+    var inputIDs = maps[mapIndex]['inputIDs'];
+    var hashmap = new Map();
+    var inputIDIndex = 0;
+    hashmap["variable"] = 0;
+    hashmap["lowerBoundary"] = 0;
+    hashmap["upperBoundary"] = 0;
+    hashmap["increment"] = 0;
+    hashmap["command"] = 0;
+    for(var j = 0; j < text.length; ++j){
+        for(var i = 0 ; i < text[j].length; ++i){
+            if(text[j][i] == '#'){
+                var inputElement = document.createElement("input");
+                inputElement.setAttribute("type", "text");
+                inputElement.classList.add("replaceInput");
+                inputElement.id = inputIDs[inputIDIndex]+hashmap[inputIDs[inputIDIndex]];
+                hashmap[inputIDs[inputIDIndex]] = hashmap[inputIDs[inputIDIndex]] + 1;
+                replaceCodeField.appendChild(inputElement);
+                inputIDIndex += 1;
+            }else if(text[j][i] == '&'){
+                field.innerHTML += "<br>";
+            }else if(text[j][i] == '%'){
+                field.innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;"
+            }else{
+                field.innerHTML = field.innerHTML + text[j][i];
+            }
+        }
+        field.innerHTML += "<br>"; 
+        
+        
+    }
+    
+}
+
+function setDifficulty(difficulty){
+    if(difficulty == "beginner"){
+        if(maps[mapIndex]["command"][0] == "ifstatement"){
+            document.getElementById("forwardButton").style.visibility = "hidden";
+            document.getElementById("backwardButton").style.visibility = "hidden";
+            document.getElementById("turnleftButton").innerHTML = "turn left";
+            document.getElementById("turnrightButton").innerHTML = "turn right";
+
+            var inputField = document.getElementById("inputField");
+            for(var element of maps[mapIndex]["baseText"]){
+                inputField.value = inputField.value + element +"\n";
+            }
+        }else{
+            document.getElementById("forwardButton").innerHTML = "forward";
+            document.getElementById("backwardButton").innerHTML = "turn backward";
+            document.getElementById("turnleftButton").innerHTML = "turn left";
+            document.getElementById("turnrightButton").innerHTML = "turn right";
+        }
+    }else if(difficulty == "advanced"){
+        document.getElementById("forwardButton").innerHTML = "X = X + 1";
+        document.getElementById("backwardButton").innerHTML = "X = X - 1";
+        document.getElementById("turnleftButton").innerHTML = "Y = Y + 1";
+        document.getElementById("turnrightButton").innerHTML = "Y = Y - 1";
+    }
+}
+
+function setMapIndex(index){
+    mapIndex = index;
+}
+
+function saveScoreToDatabase(userId, mapId, score){
+    console.log("SAVING DATA");
+    var data = new FormData();
+    data.append('userId', userId);
+    data.append('mapId', mapId);
+    data.append('score',score);
+    var httpr = new XMLHttpRequest();
+    httpr.open("POST","saveScoreInDB.php",true);
+    httpr.onload = function (){
+        console.log(this.repsonse);
+    }
+    httpr.send(data);
 }
